9fe04a9784c3acdcd5a6be72c88fccd3a2356bd8
freencis <freencis@gmail.com>
2015-09-22 19:41:58 +0000

    Start

---
+++ application/views/pages/home.php
@@ -0,0 +1,4 @@
+<!DOCTYPE html>
+<html>
+    <h1>See</h1>
+</html>
---
+++ application/views/templates/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> teste </title>
+    </head>
+    <body>
+        <h1>
+            print:
+        </h1>
+        <?php echo $title; ?>
+    </body>
+</html>
