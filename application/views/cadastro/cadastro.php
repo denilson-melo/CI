6287ac642a9010d31bc7662085dcfcd3e31d583e
Meganium\Denilson <freencis@gmail.com>
2015-09-25 01:56:10 -0300

    Icons

--- application/views/cadastro/cadastro.php
+++ application/views/cadastro/cadastro.php
@@ -2,21 +2,25 @@
 	<?php echo form_open('cadastro'); ?>
 	
 			<div class="input-field">
+				<i class="material-icons prefix">account_circle</i>
 				<label for="nome">Nome</label>
 				<input type="text" name="nome" class="col s16"/>
 			</div>
 			
 			<div class="input-field">
+				<i class="material-icons prefix">email</i>
 				<label for="email">Email</label>
 				<input type="email" name="email"/>
 			</div>
 
 			<div class="input-field">
+				<i class="material-icons prefix">lock_outline</i>
 				<label for="senha">Senha</label>
 				<input type="password" name="senha"/>
 			</div>
 
 			<div class="input-field">
+				<i class="material-icons prefix">lock</i>
 				<label for="senha_validar">Confirmar Senha</label>
 				<input type="password" name="senha_validar"/>
 			</div>
@@ -30,6 +34,7 @@
 			</div>
 
 			<div class="input-field">
+				<i class="material-icons prefix">phone</i>
 				<label for="telefone">Telefone</label>
 				<input type="text" name="telefone"/>
 			</div>
